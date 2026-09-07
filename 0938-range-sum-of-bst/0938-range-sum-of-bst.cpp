6230b5eb65c436b0d50189d89ac29191127c1d8a
Lakshya Pratap Singh <lakshyapratapsingh9935@gmail.com>
2026-09-07 16:26:03 +0530

    Time: 0 ms (100%), Space: 65 MB (61.06%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -6,18 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if(root == NULL) return 0;
-        if(low <= root->val && high >= root->val){
-            return root->val + rangeSumBST(root->left, low, high)+ rangeSumBST(root->right, low, high);
-        }else if (root -> val < low){
+        if (root == NULL)
+            return 0;
+        if (low <= root->val && high >= root->val) {
+            return root->val + rangeSumBST(root->left, low, high) +
+                   rangeSumBST(root->right, low, high);
+        } else if (root->val < low) {
             return rangeSumBST(root->right, low, high);
-        }else{
+        } else {
             return rangeSumBST(root->left, low, high);
         }
     }
