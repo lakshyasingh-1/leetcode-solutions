a9879ca8f0d3d43a00d4079293b190c68d0f6844
Lakshya Pratap Singh <lakshyapratapsingh9935@gmail.com>
2026-09-07 16:29:18 +0530

    Time: 0 ms (100%), Space: 65 MB (30.7%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -15,13 +15,13 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if (root == NULL)
             return 0;
-        if (low <= root->val && high >= root->val) {
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        } else if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        } else {
             return root->val + rangeSumBST(root->left, low, high) +
                    rangeSumBST(root->right, low, high);
-        } else if (root->val < low) {
-            return rangeSumBST(root->right, low, high);
-        } else {
-            return rangeSumBST(root->left, low, high);
         }
     }
 };
