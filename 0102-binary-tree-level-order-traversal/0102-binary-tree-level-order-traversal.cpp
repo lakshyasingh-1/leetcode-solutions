ab9f05f2576caaf52dc5cec9e299d209bec1615b
Lakshya Pratap Singh <lakshyapratapsingh9935@gmail.com>
2026-09-07 15:08:05 +0530

    Time: 0 ms (100%), Space: 16.9 MB (90.72%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,43 +6,43 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-
-        if (root == nullptr)
+        vector<vector<int>>ans;
+        if(root==NULL)
+        {
             return ans;
-
-        queue<TreeNode*> q;
+        }
+        
+        queue<TreeNode*>q;
         q.push(root);
-
-        while (!q.empty()) {
-
-            int size = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode* curr = q.front();
-                q.pop();
-
-                level.push_back(curr->val);
-
-                if (curr->left != nullptr)
-                    q.push(curr->left);
-
-                if (curr->right != nullptr)
-                    q.push(curr->right);
+        q.push(NULL);
+        vector<int>temp;
+        while(!q.empty())
+        {
+            TreeNode*frontN=q.front();
+            q.pop();
+            if(frontN==NULL)
+            {
+               ans.push_back(temp);
+               temp.clear();
+               if(!q.empty())
+               {
+                 q.push(NULL);
+               }
+            }else{
+                temp.push_back(frontN->val);
+                if(frontN->left!=NULL)
+                  q.push(frontN->left);
+                if(frontN->right!=NULL)
+                  q.push(frontN->right);
             }
 
-            ans.push_back(level);
         }
-
         return ans;
     }
 };
