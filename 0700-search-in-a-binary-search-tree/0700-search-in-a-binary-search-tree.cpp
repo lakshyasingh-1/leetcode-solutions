58885002a73e16fceedd6dc41f03aefae1f1f2ef
Lakshya Pratap Singh <lakshyapratapsingh9935@gmail.com>
2026-09-14 12:39:12 +0530

    Time: 0 ms (100%), Space: 21.8 MB (99.9%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -6,18 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int k) {
-        while(root != NULL){
-            if(root -> val == k) return root;
-            if(root -> val > k) root = root -> left;
-            else root = root -> right;
+        while (root != NULL) {
+            if (root->val == k)
+                return root;
+            if (root->val > k)
+                root = root->left;
+            else
+                root = root->right;
         }
         return NULL;
-        
     }
 };
+
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
+}
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}
